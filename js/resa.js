d0fcd0efaf63c2cba68ba233b93cd51e5590ff0c
JustineDev00 <dev.justine.verin@gmail.com>
2022-07-08 11:57:36 +0200

    création objets villas et ajout required sur champs

--- js/resa.js
+++ js/resa.js
@@ -1,3 +1,14 @@
+let HTMLinputfields = document.querySelectorAll("input");
+let HTMLselects = document.querySelectorAll("select");
+for (const input of HTMLinputfields) {
+    input.setAttribute("required", true);
+}
+
+for (const select of HTMLselects) {
+    select.setAttribute("required", true);
+}
+
+
 let dateDebutResas = new Date(2022, 06, 17).toISOString().slice(0,10);
 let dateFinResas = new Date(2022, 08, 3).toISOString().slice(0,10);
 
@@ -8,5 +19,16 @@ calendrier.step = 7;
 
 
 
-let villa1 = {};
-let villa2 = {};
+let villa1 = {
+    villaDebutResa: dateDebutResas,
+    villaFinResa: dateFinResas,
+};
+let villa2 = {
+    villaDebutResa: dateDebutResas,
+    villaFinResa: dateFinResas,
+};
+
+let dureeResa = document.querySelector("#dureeSejour").value;
+let choixVilla = document.querySelector("#choixVilla").value;
+
+let bp = true;
