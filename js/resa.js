0b340c6b6f67a122e5c4dcb13af5a37a8fb0419f
JustineDev00 <dev.justine.verin@gmail.com>
2022-07-11 15:39:33 +0200

    paramétré l'affichage des champs de saisie numéro CB en fonction de la carte choisie

--- js/resa.js
+++ js/resa.js
@@ -1,19 +1,66 @@
 let HTMLinputfields = document.querySelectorAll("input");
 let HTMLselects = document.querySelectorAll("select");
 
+//Paramétrer l'affichage des champs HTML selon le type de carte choisi
+let lignePrincipale = document.querySelector("#lignePrincipale");
+let CBInputField = document.querySelector("#CBInputfield");;
+let visaInputField = document.querySelector("#visaInputField");
+let MCInputField = document.querySelector("#MCInputField");
+let AmExpInputField = document.querySelector("#AmExpInputField");
+
+visaInputField.remove();
+MCInputField.remove();
+AmExpInputField.remove();
 
 
-let dateDebutResas = new Date(2022, 06, 17).toISOString().slice(0,10);
+
+let selectionCarte = document.querySelector("#carteBancaire");
+
+
+selectionCarte.onchange = function() {
+let modeleDeCarte = document.querySelector("#carteBancaire").value;
+switch (modeleDeCarte) {
+    case "1":
+        visaInputField.remove();
+        MCInputField.remove();
+        AmExpInputField.remove();
+        lignePrincipale.insertBefore(CBInputField, document.querySelector("#ExpirationDate"));
+        
+        break;
+    case "2":
+        CBInputField.remove();
+        MCInputField.remove();
+        AmExpInputField.remove();
+        lignePrincipale.insertBefore(visaInputField, document.querySelector("#ExpirationDate"));
+        break;
+    case "3":
+        CBInputField.remove();
+        visaInputField.remove();
+        AmExpInputField.remove();
+        lignePrincipale.insertBefore(MCInputField, document.querySelector("#ExpirationDate"));
+        break;
+    case "4":
+        CBInputField.remove();
+        MCInputField.remove();
+        visaInputField.remove();
+        lignePrincipale.insertBefore(AmExpInputField ,document.querySelector("#ExpirationDate"));
+        break;
+    default:
+        break;
+;}}
+
+
+let dateDebutResas = new Date(2022, 06, 17).toISOString().slice(0, 10);
 //mettre if statement pour calculer nouvelle date de début universelle en fonction de la date actuelle si date après 16 juillet
-let dateFinResas = new Date(2022, 08, 3).toISOString().slice(0,10);
+let dateFinResas = new Date(2022, 08, 3).toISOString().slice(0, 10);
 
 let calendrierV1 = document.querySelector("#dateArriveeV1");
-calendrierV1.max = dateFinResas; 
+calendrierV1.max = dateFinResas;
 calendrierV1.min = dateDebutResas;
 calendrierV1.step = 7;
 
 let calendrierV2 = document.querySelector("#dateArriveeV2");
-calendrierV1.max = dateFinResas; 
+calendrierV1.max = dateFinResas;
 calendrierV1.min = dateDebutResas;
 calendrierV1.step = 7;
 
@@ -31,7 +78,7 @@ let dureeResa = document.querySelector("#dureeSejour").value;
 let choixVilla = document.querySelector("#choixVilla").value;
 
 let formulaire = document.querySelector("#form");
-formulaire.onsubmit = function()  {
+formulaire.onsubmit = function () {
     alert("formulaire valide");
     window.open("validation.html");
 };
