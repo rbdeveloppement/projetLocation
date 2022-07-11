b284b7bf01f1807e3755cc51b6068c2ddcf96000
rbdeveloppement <rbdeveloppement@gmail.com>
2022-07-11 11:41:10 +0200

    maj resa

--- js/validation_cb_et_cp.js
+++ js/validation_cb_et_cp.js
@@ -19,7 +19,7 @@ document.querySelector(".btn").onclick = (evt) => {
     /^(?:(?:\+|00)33[\s.-]{0,3}(?:\(0\)[\s.-]{0,3})?|0)[1-9](?:(?:[\s.-]?\d{2}){4}|\d{2}(?:[\s.-]?\d{3}){2})$/;
   if (regextel.test(telephone) == false) {
     document.querySelector(".msg_tel").innerText =
-      "numéro de téléphone invalide";
+      "numéro de téléphone invalide"; 
   }
   // vérification mail
   let adresseMail = document.querySelector("#mail").value;
