1199d336cd3aeb4c88410e1ed30015cd5e9bbb6b
rbdeveloppement <rbdeveloppement@gmail.com>
2022-07-13 15:39:44 +0200

    OK

--- js/resa.js
+++ js/resa.js
@@ -1,123 +1,160 @@
+const DateTime = easepick.DateTime;
+
 let HTMLinputfields = document.querySelectorAll("input");
 let HTMLselects = document.querySelectorAll("select");
 
+//Récupération des données de la session précédente
+
+//1.Récupérer numéro de la réservation
+let numeroReservationJSON = localStorage.getItem("numeroReservation");
+let numeroReservation = JSON.parse(numeroReservationJSON);
+if (numeroReservation == null) {
+    numeroReservation = 0;
+}
+
+//2.récupérer l'ensemble des informations enregistrées dans localStorage
+let localStorageData = [];
+localStorageData.push(Object.entries(localStorage));
+
+
+// //Parse les clés et valeurs pour convertir en JS strings
+// for (let i = 0; i < localStorageData.length; i++) {
+//     const element = localStorageData[i];
+//     for (let j = 0; j < element.length; j++) {
+//         const item = JSON.parse(element[j]);
+//     }
+// }
+
+//Recupérer et ajouter les dates dans les tableaux de réservation
+
+let bookedDatesV1 = []
+let bookedDatesV2 = [];
+let bookedDates = bookedDatesV1;   //Bonne nouvelle, j'ai résolu ce bug
+
+localStorageData = localStorageData[0]
+
+
+if(localStorageData.length > 0){
+for (let i = 0; i < localStorageData.length; i++) {
+    if (localStorageData[i][0].startsWith('maisonChoisie') && localStorageData[i][1] == 1) {
+        let datesAsStrings = localStorageData[i + 1][1];
+        let datesArray = datesAsStrings.split(" au ");
+        bookedDatesV1Strings.push(datesArray);
+    }
+    else if (localStorageData[i][0].startsWith('maisonChoisie') && localStorageData[i][1] == 2) {
+        let datesAsStrings = localStorageData[i + 1][1];
+        let datesArray = datesAsStrings[1].split(" au ");
+        bookedDatesV2Strings.push(datesArray);
+    }
+    else {
+        continue;
+    }
+}}
+
+bookedDatesV1.map(d => {
+    if (d instanceof Array){
+        const start = new DateTime(d[0], 'DD-MM-YYYY');
+        const end = new DateTime(d[1], 'DD-MM-YYYY');
+        return [start, end];
+    }
+    return new DateTime(d, "DD-MM-YYYY");
+
+} );
+
+bookedDatesV2.map(d => {
+    if (d instanceof Array){
+        const start = new DateTime(d[0], 'DD-MM-YYYY');
+        const end = new DateTime(d[1], 'DD-MM-YYYY');
+        return [start, end];
+    }
+    return new DateTime(d, "DD-MM-YYYY");
+
+} );
+
+
+let choixVilla = document.getElementById("choixVilla");
+choixVilla.onchange = () => {
+    if (choixVilla.value = 1) {
+        bookedDates = bookedDatesV1;
+    }
+    else {
+        bookedDates = bookedDatesV2;
+    }
+}
 
 //Active le champ CB lorsqu'une valeur de carte est choisie:
 let CBfield = document.getElementById("numeroCB");
 let choixModele = document.getElementById("carteBancaire")
 
-choixModele.onchange = function(){
-    if (choixModele.value != "CB"){
+choixModele.onchange = function () {
+    if (choixModele.value != "CB") {
         CBfield.disabled = false;
     }
-    else{
+    else {
         CBfield.disabled = true;
     }
-   
-    
-};
 
-//Evènement lorsque formulaire valide
-
-// let formulaire = document.querySelector("#form");
-// formulaire.onsubmit = function(e){
-//     e.preventDefault();}
-
-function testValidation(){
-  if (telIsOk && cpIsOk && mailIsOk && visaIsOk && masterIsOk && AmExpisOk && dateCB == true){
-    alert("formulaire valide");
-    // window.open("validation.html");
-  }}
 
 
 //Calendriers, dates, etc
 
 let datMinV1 = new Date("2022-07-16"), datMinV2 = datMinV1;
-let datMaxV1 = new Date("2022-08-27"); datMaxV2 = datMaxV1;
-
-let listeSamedisV1 = [
-    '16-07-2022', '23-07-2022', '30-07-2022', '06-08-2022', '13-08-2022', '20-08-2022', '27-08-2022'
-];
-let listeSamedisV2 = listeSamedisV1;
-
-let changeMin = function(MinDate){
-    let ajd = new Date();
-    let firstDateAvailable = ajd;
-    if (firstDateAvailable.getTime() - MinDate.getTime() >= 0) {
-    let ajdJour = firstDateAvailable.getDay();
-    if (ajdJour === 6) {
-        MinDate = new Date(firstDateAvailable.getTime() + 7 * msPerDay);
-    }
-    else {
-        MinDate = new Date((firstDateAvailable.getTime() + (7 - ajdJour)) * msPerDay);
-    }
-}
-}
-
-changeMin(datMinV1);
-changeMin(datMinV2);
+let datMaxV1 = new Date("2022-08-27"), datMaxV2 = datMaxV1;
 
 
 //calendriers = modifier les variables associées aux clés AVANT l'exécution de ce code!
-const pickerVilla1 = new easepick.create({
-    element: document.getElementById('dateArriveeV1'),
+const pickerVilla = new easepick.create({
+    element: document.getElementById('dateArrivee'),
     css: [
         'https://cdn.jsdelivr.net/npm/@easepick/bundle@1.2.0/dist/index.css',
     ],
     lang: 'fr-FR',
     format: 'DD-MM-YYYY',
     plugins: ['LockPlugin', 'RangePlugin'],
-    LockPlugin: { minDate: datMinV1, maxDate: datMaxV1, inseparable: false, minDays: 8, maxDays: 29,
-    filter(date, picked){return !listeSamedisV1.includes(date.format('DD-MM-YYYY'));} },
-    RangePlugin: {delimiter: " au ",}
+    LockPlugin: {
+        filter(date, picked){
+            if (picked.length === 1) {
+                const incl = date.isBefore(picked[0]) ? "[)" : "(]";
+                return (
+                  !picked[0].isSame(date, "day") && date.inArray(bookedDates, incl));
+              }
+              return date.inArray(bookedDates, "[)");
+        },
+    minDate: datMinV1, maxDate: datMaxV1, inseparable: true, minDays: 7, maxDays: 28,
+    },
+    RangePlugin: { delimiter: " au ", }
 });
 
-const pickerVilla2 = new easepick.create({
-    element: document.getElementById('dateArriveeV2'),
-    css: [
-        'https://cdn.jsdelivr.net/npm/@easepick/bundle@1.2.0/dist/index.css',
-    ],
-    lang: 'fr-FR',
-    format: 'DD-MM-YYYY',
-    plugins: ['LockPlugin', 'RangePlugin'],
-    LockPlugin: { minDate: datMinV2, maxDate: datMaxV2, inseparable: false, minDays: 8, maxDays: 29,
-    filter(date, picked){return !listeSamedisV1.includes(date.format('DD-MM-YYYY'));} },
-    RangePlugin: {delimiter: " au ",}
-});
+//a insérer dans LockPlugin
+// filter(date, picked){return !listeSamedisV1.includes(date.format('DD-MM-YYYY'));} 
+
+
+
+//Evènement lorsque formulaire valide
+// let formulaire = document.querySelector("#form");
+// formulaire.onsubmit = (event) => {
+//     event.preventDefault();}
+
+function Validationformulaire(){
+    alert("formulaire valide");
+    window.open("validation.html");
+    //Sauvegarde des données
+    let nom = document.querySelector("#nom").value;
+    localStorage.setItem(`nom${numeroReservation}`, nom);
+    let prenom = document.querySelector("#prenom").value;
+    localStorage.setItem(`prenom${numeroReservation}`, prenom);
+    let telephone = document.querySelector("#tel").value;
+    localStorage.setItem(`telephone${numeroReservation}`, telephone);
+    let maisonChoisie = document.querySelector("#choixVilla").value;
+    localStorage.setItem(`maisonChoisie${numeroReservation}`, maisonChoisie);
+    let datesDeResas = document.querySelector("#dateArrivee").value;
+    localStorage.setItem(`datesDeResas${numeroReservation}`, datesDeResas);
+    numeroReservation++;
+    localStorage.setItem("numeroReservation", JSON.stringify(numeroReservation));
+    let bp = true;
+
+};
 
 
 
 
-// let dateDebutResas = new Date(2022, 06, 17).toISOString().slice(0, 10);
-// //mettre if statement pour calculer nouvelle date de début universelle en fonction de la date actuelle si date après 16 juillet
-// let dateFinResas = new Date(2022, 08, 3).toISOString().slice(0, 10);
-
-// let calendrierV1 = document.querySelector("#dateArriveeV1");
-// calendrierV1.max = dateFinResas;
-// calendrierV1.min = dateDebutResas;
-// calendrierV1.step = 7;
-
-// let calendrierV2 = document.querySelector("#dateArriveeV2");
-// calendrierV1.max = dateFinResas;
-// calendrierV1.min = dateDebutResas;
-// calendrierV1.step = 7;
-
-//Objets??
-
-// let villa1 = {
-//     villaDebutResa: dateDebutResas,
-//     villaFinResa: dateFinResas,
-// };
-// let villa2 = {
-//     villaDebutResa: dateDebutResas,
-//     villaFinResa: dateFinResas,
-// };
-
-// let dureeResa = document.querySelector("#dureeSejour").value;
-// let choixVilla = document.querySelector("#choixVilla").value;
-
-
-
-
-
-let bp = true;
