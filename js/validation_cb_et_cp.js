14761664bac8a8e073e3e1b6955f69ab7fb862e9
JustineDev00 <dev.justine.verin@gmail.com>
2022-07-18 17:36:14 +0200

    résolu pb validation formulaire

--- js/validation_cb_et_cp.js
+++ js/validation_cb_et_cp.js
@@ -1,26 +1,29 @@
 
 //verification date de validité de la CB
+let cpIsOk = false;
+let telIsOk = false;
+let mailIsOk = false;
+let visaIsOk = false;
+let masterIsOk = false;
+let AmExpisOk = false;
+let dateCB = false;
 
-document.querySelector(".btn").onclick = (evt) => {
-    evt.preventDefault();
-let moisValide = document.querySelector("#moisExpCarte").value;
-let anneeValide = document.querySelector("#annee").value;
-    if(anneeValide == 2022 && moisValide < 07 || anneeValide < 2022){
-        document.querySelector(".msg_cbDate").innerText ="date d'invalidité atteinte";
-    }else{
-        dateCB = true;
-    }
-}
 
-let cpIsOk = true;
-let telIsOk = true;
-let mailIsOk = true;
-let visaIsOk = true;
-let masterIsOk = true;
-let AmExpisOk = true;
-let dateCB = true;
 
-document.querySelector("#form").onsubmit = (evt) => {
+// document.querySelector(".btn").onclick = (evt) => {
+//     evt.preventDefault();
+// let moisValide = document.querySelector("#moisExpCarte").value;
+// let anneeValide = document.querySelector("#annee").value;
+//     if(anneeValide == 2022 && moisValide < 07 || anneeValide < 2022){
+//         document.querySelector(".msg_cbDate").innerText ="date d'invalidité atteinte";
+//     }else{
+//         dateCB = true;
+//     }
+// }
+
+
+
+document.getElementById("form").onsubmit = (evt) => {
   evt.preventDefault();
   //verification code postal
   let codePostal = document.getElementById("codePostale").value;
@@ -29,6 +32,10 @@ document.querySelector("#form").onsubmit = (evt) => {
   if (cpIsOk == false) {
     document.querySelector(".msg_Cp").innerText = "code postal invalide";
   }
+  else{
+    cpIsOk = true;
+    document.querySelector(".msg_Cp").innerText = "";
+  }
 
   // vérification téléphone
   let telephone = document.getElementById("tel").value;
@@ -36,8 +43,11 @@ document.querySelector("#form").onsubmit = (evt) => {
     /^(?:(?:\+|00)33[\s.-]{0,3}(?:\(0\)[\s.-]{0,3})?|0)[1-9](?:(?:[\s.-]?\d{2}){4}|\d{2}(?:[\s.-]?\d{3}){2})$/;
   telIsOk = regextel.test(telephone);
   if (telIsOk == false) {
-    document.querySelector(".msg_tel").innerText =
-      "numéro de téléphone invalide";
+    document.querySelector(".msg_tel").innerText =  "numéro de téléphone invalide";
+  }
+  else{
+    telIsOk = true;
+    document.querySelector(".msg_tel").innerText = "";
   }
   // vérification mail
   let adresseMail = document.getElementById("mail").value;
@@ -47,21 +57,35 @@ document.querySelector("#form").onsubmit = (evt) => {
   if (mailIsOk == false) {
     document.querySelector(".msg_mail").innerText = "adresse e-mail invalide";
   }
+  else{
+    mailIsOk = true;
+    document.querySelector(".msg_mail").innerText = "";
+  }
 
   // verification carte bancaire
   let carteBancaire = document.getElementById("carteBancaire").value;
   let numCB = document.getElementById("numeroCB").value;
+  let numCBisOk = false;
   if (carteBancaire == "Visa") {
     let regexVisa = /^4\d{3}(| |-)(?:\d{4}\1){2}\d{4}$/;
     visaIsOk = regexVisa.test(numCB);
     if (visaIsOk == false) {
       document.querySelector(".msg_CB").innerText = "carte visa invalide";
     }
+    else{
+      numCBisOk = true;
+      document.querySelector(".msg_CB").innerText = ""
+    }
+
   } else if ((carteBancaire = "MasterCard")) {
     let regexMaster = /^5[1-5]\d{2}(| |-)(?:\d{4}\1){2}\d{4}$/;
     masterIsOk = regexMaster.test(numCB);
     if (masterIsOk == false) {
       document.querySelector(".msg_CB").innerText = "carte mastercard invalide";
+    }
+    else{
+      numCBisOk = true;
+      document.querySelector(".msg_CB").innerText = ""
     }
   } else if ((carteBancaire = "AmExpress")) {
     let regexAE = /^3[47]\d{1,2}(| |-)\d{6}\1\d{6}$/;
@@ -69,6 +93,10 @@ document.querySelector("#form").onsubmit = (evt) => {
     if (AmExpisOk == false) {
       document.querySelector(".msg_CB").innerText =
         "carte american express invalide";
+    }
+    else{
+      numCBisOk = true;
+      document.querySelector(".msg_CB").innerText = ""
     }
   }
   let moisValide = document.querySelector("#moisExpCarte").value;
@@ -80,7 +108,14 @@ document.querySelector("#form").onsubmit = (evt) => {
     }
    else {
     dateCB = true;
+    document.querySelector(".msg_cbDate").innerText = "";
   }
-  testValidation();
+
+  if(cpIsOk && telIsOk && mailIsOk && numCBisOk && dateCB ){
+    Validationformulaire();
+    evt.returnValue = true;
+  }
+  
+  
 };
 
